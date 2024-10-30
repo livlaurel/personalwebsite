49213ce4ed6331d404c205c83716e6bfee8fcd88
livlaurel <OLaurel113@gmail.com>
2024-10-30 00:26:36 -0500

    Moving name into circle

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,7 +5,9 @@ import PersProjects from './components/PersProjects/personalpro';
 function Home() {
   return (
     <div className="Home">
-      <div className="circle"></div>
+      <div className="circle">
+        <h2 className="name">Olivia Laurel</h2>
+      </div>
       <div className="blob top-left-blob"></div> 
       <div className="blob two-top-left-blob"></div> 
       <div className="blob three-top-left-blob"></div> 
@@ -13,14 +15,11 @@ function Home() {
       <div className="blob two-bottom-right-blob"></div> 
       <div className="blob three-bottom-right-blob"></div> 
       <header className="Home-header">
-        <div className="header-box">
-          <h2>Olivia Laurel</h2>
           <nav className="header-links">
             <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">GitHub</a>
             <a href="https://www.linkedin.com/in/your-linkedin-username" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             <Link to="/blog">Blog</Link> 
           </nav>
-        </div>
       </header>
       <div className='PersonalProjects'>
         <PersProjects/>
