3f55b96a25fca4b06838d888c38ec6b3ba15ddaf
livlaurel <OLaurel113@gmail.com>
2024-11-28 00:48:48 -0600

    adding art sections

--- src/gallery/gallery.tsx
+++ src/gallery/gallery.tsx
@@ -9,11 +9,14 @@ import spaceImage from './space.jpg';
 import wonderImage from './wonder.jpg';
 import './gallery.css';
 
-const images = [
+const watercolorImages = [
   { id: 1, src: beaImage, title: 'Bea' },
   { id: 2, src: aptImage, title: 'Apartment' },
   { id: 3, src: womanImage, title: 'Happy' },
   { id: 4, src: fisImage, title: 'Fish' },
+];
+
+const wonderImages = [
   { id: 5, src: appleImage, title: 'apple' },
   { id: 6, src: spaceImage, title: 'space' },
   { id: 7, src: wonderImage, title: 'wonder' },
@@ -23,12 +26,27 @@ const Gallery: React.FC = () => {
   return (
     <div className="Gallery">
       <h1>Art Gallery</h1>
-      <div className="gallery-grid">
-        {images.map(image => (
-          <Link key={image.id} to={`/gallery/${image.id}`}>
-            <img src={image.src} alt={image.title} />
-          </Link>
-        ))}
+      
+      <div className="gallery-section">
+        <h2>Water Color</h2>
+        <div className="gallery-grid">
+          {watercolorImages.map(image => (
+            <Link key={image.id} to={`/gallery/${image.id}`}>
+              <img src={image.src} alt={image.title} />
+            </Link>
+          ))}
+        </div>
+      </div>
+      
+      <div className="gallery-section">
+        <h2>Wonder</h2>
+        <div className="gallery-grid">
+          {wonderImages.map(image => (
+            <Link key={image.id} to={`/gallery/${image.id}`}>
+              <img src={image.src} alt={image.title} />
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
