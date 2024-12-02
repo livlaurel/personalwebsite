d566d1e4819e4c4053d4282688a2aa8db7ae21e1
livlaurel <OLaurel113@gmail.com>
2024-12-02 16:33:04 -0600

    about me page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Gallery from './gallery/gallery';
 import ImageDetail from './gallery/picture';
+import About from './about/about';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/gallery/:id" element={<ImageDetail />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/about/about.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './about.css';
+
+const About: React.FC = () => {
+  return (
+    <div className="about-container">
+      <h1>About Me</h1>
+      <p>Hello! I'm Olivia Laurel!</p>
+      <p>I'm currently an undergrad CS major and I hope to go into web deisgn. I love to draw in my free time (check out the art gallery ;p). I made this website as a way to document my work and growth in software dev!</p>
+    </div>
+  );
+};
+
+export default About;
