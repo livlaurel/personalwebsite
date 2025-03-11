3aa92be63ba95fea066447cc9d8721ebb0cca570
livlaurel <OLaurel113@gmail.com>
2025-03-10 23:53:40 -0500

    setting things up

---
+++ personalweb/src/pages/home.tsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+    return (
+        <div className='flex flex-col h-screen'>
+            <main className="bg-[#fff9f0] flex-grow p-7">
+                <section className="container mx-auto">
+                    <div className="box box-content rounded-sm text-base p-4 h-110 bg-[#9caf88]">
+                    </div>
+                </section>
+            </main>
+        </div>
+    )
+}
+
+export default Home;
