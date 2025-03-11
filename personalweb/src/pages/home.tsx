0b398da7cf080418955a14807afa10407efcd852
livlaurel <OLaurel113@gmail.com>
2025-03-11 09:27:39 -0500

    boxes added

--- personalweb/src/pages/home.tsx
+++ personalweb/src/pages/home.tsx
@@ -6,7 +6,13 @@ const Home = () => {
             <Header />
             <main className="bg-[#fff9f0] flex-grow p-7">
                 <section className="container mx-auto">
-                    <div className="box box-content rounded-sm text-base p-4 h-110 bg-[#9caf88]">
+                    <div className='flex justify-between items-center space-x-4'>
+                        <div className="box box-content rounded-md border-2 p-4 h-80 w-70 bg-[#9caf88] box-shadow">
+                        </div>
+                        <div className="box box-content rounded-md border-2 p-4 h-80 w-70 bg-[#9caf88] box-shadow">
+                        </div>
+                        <div className="box box-content rounded-md border-2 p-4 h-80 w-70 bg-[#9caf88] box-shadow">
+                        </div>
                     </div>
                 </section>
             </main>
