e934dda127f3a01978dc7f304ec02701227df3a1
livlaurel <OLaurel113@gmail.com>
2025-03-11 09:35:18 -0500

    added imgs

--- personalweb/src/pages/home.tsx
+++ personalweb/src/pages/home.tsx
@@ -1,4 +1,7 @@
 import Header from '../components/header';
+import pfp from '../imgs/pfp.png';
+import inspo from '../imgs/inspo.png';
+import dev from '../imgs/dev.png';
 
 const Home = () => {
     return (
@@ -6,12 +9,16 @@ const Home = () => {
             <Header />
             <main className="bg-[#fff9f0] flex-grow p-7">
                 <section className="container mx-auto">
-                    <div className='flex justify-between items-center space-x-4'>
-                        <div className="box box-content rounded-md border-2 p-4 h-80 w-70 bg-[#9caf88] box-shadow">
+                    <h1 className="text-4xl font-bold text-center">Meet Olivia Laurel...</h1>
+                    <div className='flex justify-between items-center m-10'>
+                        <div className="box box-content rounded-md border-3 p-4 h-80 w-70 bg-[#9caf88]  flex justify-center items-center box-shadow">
+                            <img src={pfp} alt="pfp" className="w-60 h-65" />
                         </div>
-                        <div className="box box-content rounded-md border-2 p-4 h-80 w-70 bg-[#9caf88] box-shadow">
+                        <div className="box box-content rounded-md border-3 p-4 h-80 w-70 bg-[#9caf88]  flex justify-center items-center box-shadow">
+                            <img src={dev} alt="inspo" className="w-60 h-65" />
                         </div>
-                        <div className="box box-content rounded-md border-2 p-4 h-80 w-70 bg-[#9caf88] box-shadow">
+                        <div className="box box-content rounded-md border-3 p-4 h-80 w-70 bg-[#9caf88] flex justify-center items-center box-shadow">
+                            <img src={inspo} alt="dev" className="w-60 h-65" />
                         </div>
                     </div>
                 </section>
