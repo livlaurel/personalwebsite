17aacca7f5757f46ce1925aec7c754cbeca577c7
livlaurel <OLaurel113@gmail.com>
2025-03-11 09:11:46 -0500

    header complete

--- personalweb/src/components/header.tsx
+++ personalweb/src/components/header.tsx
@@ -5,16 +5,16 @@ const Header = () => {
         <main className="bg-[#fff9f0] top-0 z-50">
             <nav className="flex justify-between items-center p-4">
                 <a href="/" className="text-black flex justify-left">
-                    <img src={logo} alt="livia" className="w-35 h-25" />
+                    <img src={logo} alt="livia" className="w-35 h-30" />
                 </a>
-                <div className="flex-grow flex justify-center">
-                    <div className="flex underline space-x-4">
-                        <a href="/about" className="text-black">About</a>
+                <div className="flex-grow font-[Arial, Helvetica, sans-serif] flex font-bold underline justify-center items-center">
+                    <div className="flex space-x-60">
+                        <a href="/about" className="text-black">About Me</a>
                         <a href="/projects" className="text-black">Projects</a>
-                        <a href="/inspire" className="text-black">Inspire</a>
+                        <a href="/inspire" className="text-black">Inspiration</a>
                     </div>
                 </div>
-                <div className="w-35 h-25"></div>
+                <div className="w-25 h-25"></div> 
             </nav>    
         </main>
     )
