311add2a51eadd9193fd36c195ccc29b476df6f3
livlaurel <OLaurel113@gmail.com>
2025-03-12 10:29:05 -0500

    inspo page

--- personalweb/src/components/header.tsx
+++ personalweb/src/components/header.tsx
@@ -1,17 +1,27 @@
+import React from "react";
 import logo from "../imgs/logo.png";
+import { FaUserAlt } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaLightbulb } from "react-icons/fa";
 
 const Header = () => {
     return (
         <main className="bg-[#fff9f0] top-0 z-50">
-            <nav className="flex justify-between items-center p-3">
+            <nav className="flex justify-between items-center ml-5 ">
                 <a href="/" className="text-black flex justify-left">
                     <img src={logo} alt="livia" className="w-35 h-30" />
                 </a>
-                <div className="flex-grow font-[Arial, Helvetica, sans-serif] flex font-bold underline justify-center items-center">
-                    <div className="flex space-x-60">
-                        <a href="/about" className="text-black">About Me</a>
-                        <a href="/dev" className="text-black">Projects</a>
-                        <a href="/inspire" className="text-black">Inspiration</a>
+                <div className="flex-grow font-[Arial, Helvetica, sans-serif] flex font-bold underline justify-end items-center">
+                    <div className="flex space-x-8 text-xl">
+                        <a href="/about">
+                            <FaUserAlt />                        
+                        </a>
+                        <a href="/dev" >
+                            <FaLaptopCode />
+                        </a>
+                        <a href="/inspire" >
+                            <FaLightbulb />
+                        </a>
                     </div>
                 </div>
                 <div className="w-25 h-25"></div> 
--- personalweb/src/pages/inspo.tsx
+++ personalweb/src/pages/inspo.tsx
@@ -1,13 +1,36 @@
+import React from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import pic1 from '../imgs/inspo/1.jpeg';
+import pic2 from '../imgs/inspo/2.jpeg';
+import pic3 from '../imgs/inspo/3.jpeg';
+import pic4 from '../imgs/inspo/4.jpeg';
+import pic5 from '../imgs/inspo/5.jpeg';
+import pic6 from '../imgs/inspo/6.jpeg';
+
+const images = [
+    { src: pic1, alt: 'Inspiration 1' },
+    { src: pic2, alt: 'Inspiration 2' },
+    { src: pic3, alt: 'Inspiration 3' },
+    { src: pic4, alt: 'Inspiration 4' },
+    { src: pic5, alt: 'Inspiration 5' },
+    { src: pic6, alt: 'Inspiration 6' },
+];
 
 const Inspire = () => {
     return (
         <div className='flex flex-col h-screen'>
             <Header />
             <main className="bg-[#fff9f0] flex-grow p-4">
-                <div className='flex flex-row items-start'>
-                    <div className='flex flex-col space-y-6 text-xl flex-1 ml-10 mr-30 max-w-3xl'>
+                <div className='flex flex-col justify-center items-center'>
+                    <h1 className='underline font-bold text-3xl'>My Inspirations for Life</h1>
+                    <p className='mt-5 mb-5 text-base font-mono tracking-1'>A collection of colors, people, places, and moments that spark creativity, bring joy, and keep me motivated.</p>
+                </div>
+                <div className="flex flex-col space-y-6 ml-10 h-[500px] overflow-y-auto">
+                    <div className="grid grid-cols-3 gap-4">
+                        {images.map((image, index) => (
+                            <img key={index} src={image.src} alt={image.alt} className="w-full h-auto rounded-md" />
+                        ))}
                     </div>
                 </div>
             </main>
