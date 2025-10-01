477a6b39e5c36aa082c7e7fb9bc2f9e19e19f890
Olivia Laurel <OLaurel113@gmail.com>
2025-10-01 01:13:07 -0500

    rearrange home

--- personalweb/src/pages/home.tsx
+++ personalweb/src/pages/home.tsx
@@ -10,15 +10,15 @@ const Home = () => {
             <Header />
             <main className="bg-[#fff9f0] flex-grow p-7">
                 <section className="container mx-auto">
-                    <h1 className="text-4xl font-bold text-[#1a2411] font-mono tracking-wider text-center">"Meet Olivia Laurel..."</h1>
+                    <h1 className="text-4xl font-bold text-[#1a2411] font-mono tracking-wider text-center">Meet Olivia Laurel...</h1>
                     <div className='flex justify-between items-center m-10'>
-                        <a href='#/about' className="box box-content rounded-md border-3 border-[#394a28] p-4 h-80 w-70 bg-[#9caf88] flex flex-col justify-between items-center custom-shadow hover-float">
-                            <img src={pfp} alt="pfp" className="w-60 h-65" />
-                            <h3 className="text-2xl font-semibold mt-4 text-[#1a2411] font-mono">About Me</h3>
-                        </a>
                         <a href='#/dev' className="box box-content rounded-md border-3 border-[#394a28] p-4 h-80 w-70 bg-[#9caf88] flex flex-col justify-between items-center custom-shadow hover-float">
                             <img src={dev} alt="dev" className="w-60 h-65" />
                             <h3 className="text-2xl font-semibold mt-4 text-[#1a2411] font-mono">Projects</h3>
+                        </a>
+                        <a href='#/about' className="box box-content rounded-md border-3 border-[#394a28] p-4 h-80 w-70 bg-[#9caf88] flex flex-col justify-between items-center custom-shadow hover-float">
+                            <img src={pfp} alt="pfp" className="w-60 h-65" />
+                            <h3 className="text-2xl font-semibold mt-4 text-[#1a2411] font-mono">About Me</h3>
                         </a>
                         <a href='#/inspire' className="box box-content rounded-md border-3 border-[#394a28] p-4 h-80 w-70 bg-[#9caf88] flex flex-col justify-between items-center custom-shadow hover-float">
                             <img src={inspo} alt="inspo" className="w-60 h-65" />
