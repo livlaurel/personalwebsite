62137754559cf06399ebbcd2bcd886f83bd09a3b
Olivia Laurel <OLaurel113@gmail.com>
2025-10-02 00:51:40 -0500

    new refresh icon

--- personalweb/src/pages/inspo.tsx
+++ personalweb/src/pages/inspo.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import { MdOutlineRefresh } from "react-icons/md";
 
 const images = import.meta.glob('/src/imgs/inspo/*.jpeg', { eager: true });
 
@@ -26,8 +27,8 @@ const Inspiration = () => {
                         A collection of colors, people, places, and moments that spark creativity, bring joy, and keep me motivated.
                     </p>
 
-                    <button className='text-sm mb-3 font-style: italic' onClick={() => window.location.reload()}>
-                        click here to refresh for more inspiration
+                    <button className='flex items-center text-sm mb-3 font-style: italic' onClick={() => window.location.reload()}>
+                        <MdOutlineRefresh className='mr-2' />  refresh for more inspiration
                     </button>
                 </div>
                 <div className="flex flex-col space-y-6 ml-5 mr-5 h-[435px] overflow-y-auto">
