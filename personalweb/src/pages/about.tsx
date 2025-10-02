3cfe46623cb0262f30a48986dba46b389aabf0bf
Olivia Laurel <OLaurel113@gmail.com>
2025-10-02 01:04:22 -0500

    updating text

--- personalweb/src/pages/about.tsx
+++ personalweb/src/pages/about.tsx
@@ -11,12 +11,12 @@ const About = () => {
                 <div className='flex flex-row items-start'>
                     <div className='flex flex-col space-y-6 text-xl flex-1 ml-10 mr-30 max-w-3xl'>
                         <img src={about} alt="about" className="w-100 h-25" />
-                        <p>Hi I’m Olivia!</p>
-                        <p>I'm Computer Science student and I have a passion for web design and frontend development.</p>
-                        <p>Outside of studies, I love to draw, play video games, and bake. I'm currently playing through Hollow Knight and my favorite thing to bake is brown butter cookies.</p>
-                        <p>I built this website as a way to document my journey in software development, share projects, and show my growth as a developer. </p>
-                        <p>Feel free to explore and reach out if you’d like to connect!</p>
-                        <p>📍 Based in Texas | 💻 Frontend Dev & Designer</p>
+                            <p>Hi I’m Olivia!</p>
+                            <p className='ml-3 text-lg'>I'm Computer Science student and I have a passion for web design and frontend development.</p>
+                            <p className='ml-3 text-lg'>Outside of studies, I love to draw, play video games, and bake. I'm currently playing through Hollow Knight and my favorite thing to bake is brown butter cookies.</p>
+                            <p className='ml-3 text-lg'>I built this website as a way to document my journey in software development, share projects, and show my growth as a developer. </p>
+                            <p className='ml-3 text-lg'>Feel free to explore and reach out if you’d like to connect!</p>
+                            <p>📍 Based in Texas | 💻 Frontend Dev & Designer</p>
                     </div>
                     <div className="ml-10">
                         <img src={pic1} alt="pic1" className="rounded-md w-90 h-115 box box-content box-shadow-green border-3 border-[#778965] bg-[#9caf88] flex flex-col items-center" />
--- personalweb/src/pages/inspo.tsx
+++ personalweb/src/pages/inspo.tsx
@@ -22,11 +22,11 @@ const Inspiration = () => {
             <Header />
             <main className="bg-[#fff9f0] flex-grow">
                 <div className='flex flex-col justify-center items-center'>
-                    <h1 className='underline font-bold text-3xl'>My Inspirations for Life</h1>
+                    <h1 className='underline font-bold text-[#394a28] text-5xl'>My Inspirations for Life</h1>
                     <p className='mt-5 mb-2 text-base font-mono tracking-1'>
                         A collection of colors, people, places, and moments that spark creativity, bring joy, and keep me motivated.
                     </p>
-
+                    
                     <button className='flex items-center text-sm mb-3 font-style: italic' onClick={() => window.location.reload()}>
                         <MdOutlineRefresh className='mr-2' />  refresh for more inspiration
                     </button>
