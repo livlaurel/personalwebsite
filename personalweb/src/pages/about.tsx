1f19fd67ab86a247006609b585c3a54baf8bf62b
Olivia Laurel <OLaurel113@gmail.com>
2025-10-14 17:37:03 -0500

    cookiesss

--- personalweb/src/pages/about.tsx
+++ personalweb/src/pages/about.tsx
@@ -11,9 +11,9 @@ const About = () => {
                 <div className='flex flex-col lg:flex-row items-center lg:items-start'>
                     <div className='flex flex-col space-y-6 text-lg lg:text-xl flex-1 lg:ml-10 lg:mr-30 max-w-3xl'>
                         <img src={about} alt="about" className="w-full lg:w-100 h-auto" />
-                        <p>Hi I’m Olivia!</p>
-                        <p className='ml-3 text-base lg:text-lg'>I'm a Computer Science student and I have a passion for web design and frontend development.</p>
-                        <p className='ml-3 text-base lg:text-lg'>Outside of studies, I love to draw, play video games, and bake. I'm currently playing through Hollow Knight and my favorite thing to bake is brown butter cookies.</p>
+                        <p>Hi I’m <span className="font-bold text-[#e17878]">Olivia</span>!</p>
+                        <p className='ml-3 text-base lg:text-lg'>I'm a Computer Science student and I have a passion for <span className="text-[#778965] font-semibold">web design</span> and <span className="text-[#778965] font-semibold">frontend development.</span></p>
+                        <p className='ml-3 text-base lg:text-lg'>Outside of studies, I love to draw, play video games, and bake. I'm currently playing through Hollow Knight and my favorite thing to bake is <a href='https://drive.google.com/file/d/1HNvBg0IX_yG7jqlOhc_twzikbHFyxxtt/view?usp=sharing' className="font-semibold text-[#778965]">brown butter cookies.</a></p>
                         <p className='ml-3 text-base lg:text-lg'>I built this website as a way to document my journey in software development, share projects, and show my growth as a developer. </p>
                         <p className='ml-3 text-base lg:text-lg'>Feel free to explore and reach out if you’d like to connect!</p>
                         <p>📍 Based in Texas | 💻 Frontend Dev & Designer</p>
